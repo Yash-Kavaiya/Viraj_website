cbdf7499c3e30c2029f2206a3c45fee6441ad2e8
Data Guru <135059931+ds2000201@users.noreply.github.com>
2023-12-25 11:59:21 +0000

    Done

--- app.py
+++ app.py
@@ -5,15 +5,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+@app.route('/contact-us')
+def contact():
+    return render_template('contact.html')
 @app.route('/about-us')
 def about():
-    return render_template('company.html')
-@app.route('/viraj')
-def viraj():
-    return render_template('vermora.html')
-    
-
+    return render_template('about-us.html')
+@app.route('/certificate')
+def certificate():
+    return render_template('certificate.html')
+@app.route('/products')
+def products():
+    return render_template('products.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
